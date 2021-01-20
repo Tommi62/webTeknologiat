f0e5da7653b2cd233a27decd1d6c96bed56ee4af
Tommi <tommi.vainio21@gmail.com>
2021-01-20 22:21:58 +0200

    task3

--- src/index.js
+++ src/index.js
@@ -1,104 +1,22 @@
 import Lunchmenu from './assets/sodexo-menu.json';
 import Lunchmenu2 from './assets/fazer-week-example.json';
-console.log(Lunchmenu);
-let coursesEn = [];
-let coursesFi = [];
-const box = document.querySelector('#res1');
-const button = document.querySelector('.button');
-const button2 = document.querySelector('.button2');
-const button3 = document.querySelector('.button3');
-let sorted;
-let language = false;
-let sortFi = true;
-let sortEn = true;
-let selectedLanguage;
+import LunchMenuFazerFi from './assets/fazer-week-fi.json';
+import LunchMenuFazerEn from './assets/fazer-week-en.json';
+import {changeLanguage, parseSodexoMenu} from './modules/SodexoData';
+import {createList, parseFazerMenu} from './modules/FazerData';
 
-const changeLanguage = (languages) => {
-  box.innerHTML = '';
-  for(const language of languages){
-    box.innerHTML +='<li>' + language + '</li><br>';
-  }
-};
 
-button.addEventListener('click', () => {
-  if(language){
-    changeLanguage(coursesFi);
-    language = false;
-    console.log('True');
-  }
-  else {
-    changeLanguage(coursesEn);
-    language = true;
-    console.log('False');
-  }
-});
 
-const sortArray = (menu, order) =>{
-  if(order){
-    menu.sort();
-  }
-  else{
-    menu.sort();
-    menu.reverse();
-  }
-  return menu;
-};
-
-button2.addEventListener('click', () => {
-  if(!language){
-    if(sortFi){
-      sorted = sortArray(coursesFi, sortFi);
-      changeLanguage(sorted);
-      sortFi = false;
-    }
-    else{
-      sorted = sortArray(coursesFi, sortFi);
-      changeLanguage(sorted);
-      sortFi = true;
-    }
-  }
-  else {
-      if(sortEn){
-        sorted = sortArray(coursesEn, sortEn);
-        changeLanguage(sorted);
-        sortEn = false;
-      }
-      else{
-        sorted = sortArray(coursesEn, sortEn);
-        changeLanguage(sorted);
-        sortEn = true;
-      }
-}
-});
-
-const randomDish = () => {
-  if(!language){
-    selectedLanguage = coursesFi;
-  }
-  else{
-    selectedLanguage = coursesEn;
-  }
-
-  let random = Math.floor(Math.random() * selectedLanguage.length);
-  box.innerHTML = '';
-  box.innerHTML = selectedLanguage[random];
-};
-
-button3.addEventListener('click', () => {
-  randomDish();
-});
-
-const parseSodexoMenu = (sodexoDailyMenu) => {
-  const courses = Object.values(sodexoDailyMenu);
-  for(const course of courses) {
-    coursesEn.push(course.title_en);
-    coursesFi.push(course.title_fi);
-  }
-};
-
-parseSodexoMenu(Lunchmenu.courses);
+const coursesFi = parseSodexoMenu(Lunchmenu.courses);
 changeLanguage(coursesFi);
 
+let lang = true;
+const menuFi = parseFazerMenu(LunchMenuFazerFi.LunchMenus[0].SetMenus, lang);
+lang = false;
+parseFazerMenu(LunchMenuFazerEn.LunchMenus[0].SetMenus, lang);
+createList(menuFi);
+
+/*
 //Week2-Task2
 
 const meals = [
@@ -154,7 +72,7 @@ const filterVeganMeals = (setMenus) => {
 
 const veganMeals = filterVeganMeals(Lunchmenu2.LunchMenus[0].SetMenus);
 console.log('Vegan meals: ' + veganMeals);
-
+*/
 
 
 
---
+++ src/modules/FazerData.js
@@ -0,0 +1,103 @@
+let menuEn = [];
+let menuFi = [];
+const box = document.querySelector('#res2');
+const button = document.querySelector('.fazerButton');
+const button2 = document.querySelector('.fazerButton2');
+const button3 = document.querySelector('.fazerButton3');
+let sorted;
+let language = false;
+let sortFi = true;
+let sortEn = true;
+let selectedLanguage;
+
+const createList = (languages) => {
+  box.innerHTML = '';
+  for(const language of languages){
+    box.innerHTML +='<li>' + language + '</li><br>';
+  }
+};
+
+button.addEventListener('click', () => {
+  if(language){
+    createList(menuFi);
+    language = false;
+    console.log('True');
+  }
+  else {
+    createList(menuEn);
+    language = true;
+    console.log('False');
+  }
+});
+
+const sortArray = (menu, order) =>{
+  if(order){
+    menu.sort();
+  }
+  else{
+    menu.sort();
+    menu.reverse();
+  }
+  return menu;
+};
+
+button2.addEventListener('click', () => {
+  if(!language){
+    if(sortFi){
+      sorted = sortArray(menuFi, sortFi);
+      createList(sorted);
+      sortFi = false;
+    }
+    else{
+      sorted = sortArray(menuFi, sortFi);
+      createList(sorted);
+      sortFi = true;
+    }
+  }
+  else {
+      if(sortEn){
+        sorted = sortArray(menuEn, sortEn);
+        createList(sorted);
+        sortEn = false;
+      }
+      else{
+        sorted = sortArray(menuEn, sortEn);
+        createList(sorted);
+        sortEn = true;
+      }
+}
+});
+
+const randomDish = () => {
+  if(!language){
+    selectedLanguage = menuFi;
+  }
+  else{
+    selectedLanguage = menuEn;
+  }
+
+  let random = Math.floor(Math.random() * selectedLanguage.length);
+  box.innerHTML = '';
+  box.innerHTML = selectedLanguage[random];
+};
+
+button3.addEventListener('click', () => {
+  randomDish();
+});
+
+const parseFazerMenu = (setMenus, lang) => {
+    for(const setMenu of setMenus){
+      for(const meals of setMenu.Meals){
+        if(lang){
+          menuFi.push(meals.Name);
+        }else{
+          menuEn.push(meals.Name);
+        }
+      }
+    }
+    if(lang){
+      return menuFi;
+    }
+};
+
+export {createList, parseFazerMenu};
