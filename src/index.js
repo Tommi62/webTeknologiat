331b204601e7544f4333f7f74fa8e2959cf8e540
Tommi <tommi.vainio21@gmail.com>
2021-01-28 13:29:06 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -9,64 +9,6 @@ changeLanguage(coursesFi);
 
 runParseFazerMenu();
 
-/*
-//Week2-Task2
-
-const meals = [
-  {name: 'Lingonberry jam', price: 4.00},
-  {name: 'Mushroom and bean casserole', price: 5.50},
-  {name: 'Chili-flavoured wheat', price: 3.00},
-  {name: 'Vegetarian soup', price: 4.80},
-  {name: 'Pureed root vegetable soup with smoked cheese', price: 8.00}
-];
-
-//1.
-const name = 'Ifa,l(fs af)a/f-s9';
-
-const validateName = (name) => {
-  const regEx = /^[A-ZÖÄÅ]{1}[a-zöäå0-9\s,\/\(\)._-]{4,64}$/;
-  return regEx.test(name);
-};
-
-const result = validateName(name);
-console.log('Tulos: ' + result);
-
-//2.
-  let sortPrice = meals.sort((a, b) => {
-  return b.price - a.price;
-  });
-
-console.log(sortPrice);
-
-//3.
-const filtered = meals.filter(meal => meal.price < 5);
-console.log(filtered);
-
-//4.
-const multiplied = meals.map(meal => meal.price*1.15);
-console.log(multiplied);
-
-//5.
-const reduced = meals.reduce((a, b) => ({price: a.price + b.price}));
-console.log(reduced);
-
-//Advanced
-const filterVeganMeals = (setMenus) => {
-  let filteredMenu = [];
-  for(const setMenu of setMenus){
-    for(const meals of setMenu.Meals){
-      if(meals.Diets.includes('Veg')){
-        filteredMenu.push(meals.Name);
-      }
-    }
-  }
-  return filteredMenu;
-};
-
-const veganMeals = filterVeganMeals(Lunchmenu2.LunchMenus[0].SetMenus);
-console.log('Vegan meals: ' + veganMeals);
-*/
-
 //i.
 let code = ['h','e','l','l','o'];
 
--- src/parallax.js
+++ src/parallax.js
@@ -1,18 +1,33 @@
 let mouse = document.querySelector('#mouse');
 let mouseCenter = document.querySelector('#mouse-center');
 const layerOne = document.querySelector('.layer-1');
+let onOrOff = true;
+let centerX;
+let centerY;
+let invertedX;
+let invertedY;
 
 document.addEventListener('mousemove', (event) => {
   mouse.innerHTML = 'From the top left <br> X: ' + event.clientX + ', Y: ' + event.clientY;
+  centerX = event.clientX - window.innerWidth/2;
+  centerY = -(event.clientY - window.innerHeight/2);
+  mouseCenter.innerHTML = 'From the center <br> X: ' + centerX + ', Y: ' + centerY;
 });
 
-let centerX;
-let centerY;
 document.addEventListener('mousemove', (event) => {
-  centerX = event.clientX - window.innerWidth/2;
-  centerY = event.clientY - window.innerHeight/2;
-  mouseCenter.innerHTML = 'From the center <br> X: ' + centerX + ', Y: ' + centerY;
-  layerOne.style.transform = 'translateX(' + centerX/2000 + '%) translateY(' + centerY/2000 + '%)';
+  if(onOrOff){
+    invertedX = -(event.clientX - window.innerWidth/2);
+    invertedY = -(event.clientY - window.innerHeight/2);
+    layerOne.style.transform = 'translateX(' + invertedX/2000 + '%) translateY(' + invertedY/2000 + '%)';
+  }
+});
+
+document.addEventListener('dblclick', (event) => {
+  if (onOrOff) {
+    onOrOff = false;
+  }else{
+    onOrOff = true;
+  }
 });
 
 console.log(window.innerWidth, window.innerHeight);
