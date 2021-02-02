192e7cfaaae7eb462939f820c491308286f20436
Tommi <tommi.vainio21@gmail.com>
2021-02-02 14:49:43 +0200

    animation2

--- src/Animation.js
+++ src/Animation.js
@@ -9,7 +9,20 @@ window.addEventListener('scroll', () => {
   }
 });*/
 const dog = document.querySelector('.dog');
+const bird1 = document.querySelector('.bird1');
 
 let dogTimer = setTimeout(() => {
   dog.classList.add('fromleft');
 }, 50);
+
+let birdTimer = setTimeout(() => {
+  bird1.classList.add('birdattack');
+}, 50);
+
+let birdTimer2 = setTimeout(() => {
+  bird1.classList.add('birdescape');
+}, 6000);
+
+let birdTimer3 = setTimeout(() => {
+  bird1.classList.add('birdbacktonormal');
+}, 9000);
