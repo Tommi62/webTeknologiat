d9878a55623b796576aa9435014b22f7df9fe16f
Tommi <tommi.vainio21@gmail.com>
2021-02-09 14:45:41 +0200

    nothing

--- src/Animation.js
+++ src/Animation.js
@@ -10,19 +10,40 @@ window.addEventListener('scroll', () => {
 });*/
 const dog = document.querySelector('.dog');
 const bird1 = document.querySelector('.bird1');
+const bird2 = document.querySelector('.bird2');
 
-let dogTimer = setTimeout(() => {
-  dog.classList.add('fromleft');
-}, 50);
+const runAnimation = () => {
+  let dogTimer = setTimeout(() => {
+    dog.classList.add('fromleft');
+  }, 50);
 
-let birdTimer = setTimeout(() => {
-  bird1.classList.add('birdattack');
-}, 50);
+  let birdTimer = setTimeout(() => {
+    bird1.classList.add('birdattack');
+  }, 50);
 
-let birdTimer2 = setTimeout(() => {
-  bird1.classList.add('birdescape');
-}, 6000);
+  let bird2Timer = setTimeout(() => {
+    bird2.classList.add('bird2fly');
+  }, 50);
 
-let birdTimer3 = setTimeout(() => {
-  bird1.classList.add('birdbacktonormal');
-}, 9000);
+  let birdTimer2 = setTimeout(() => {
+    bird1.classList.add('birdescape');
+  }, 6000);
+
+  let birdTimer3 = setTimeout(() => {
+    bird1.classList.add('birdbacktonormal');
+  }, 9000);
+};
+
+runAnimation();
+
+setInterval(function(){
+  dog.classList.remove('fromleft');
+  bird1.classList.remove('birdattack');
+  bird1.classList.remove('birdescape');
+  bird1.classList.remove('birdbacktonormal');
+  bird2.classList.remove('bird2fly');
+  dog.classList.add('dog');
+  bird1.classList.add('bird1');
+  bird2.classList.add('bird2');
+  runAnimation();
+}, 11000);
